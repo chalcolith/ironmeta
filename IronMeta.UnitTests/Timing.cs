22bf912210de211d196810df6e98423f1bdcebc1
Gordon Tisher <gordon@balafon.net>
2012-12-18 08:28:41 -0800

    new xunit version

---
+++ IronMeta.UnitTests/Timing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+
+namespace IronMeta.UnitTests
+{
+
+    public class Timing
+    {
+
+        [Fact(Skip = "Only for benchmarking purposes.")]
+        public void TimeParser()
+        {
+            // copy ironmeta file
+            File.Copy(@"..\..\..\IronMeta.Generator\Parser.ironmeta", @".", true);
+
+
+
+        }
+
+    }
+
+}
