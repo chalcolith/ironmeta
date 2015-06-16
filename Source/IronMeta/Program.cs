4f27d5abbea864e86367ec312a4a70fa2000608c
Gordon Tisher <gordon@balafon.net>
2015-06-16 10:49:06 -0700

    add better error reporting to command-line tool

--- Source/IronMeta/Program.cs
+++ Source/IronMeta/Program.cs
@@ -48,7 +48,11 @@ namespace IronMeta
                     }
                     else
                     {
-                        Console.Error.WriteLine("IronMeta Syntax Error: " + match.MatchState.LastError);
+                        int num, offset;
+                        var line = match.MatchState.GetLine(match.MatchState.LastErrorIndex, out num, out offset);
+                        Console.Error.WriteLine("{0}({1},{2}): error: {3}", inputInfo.FullName, num, offset, match.MatchState.LastError);
+                        Console.Error.WriteLine("{0}", line);
+                        Console.Error.WriteLine("{0}^", new string(' ', offset));
                         return 1;
                     }
                 }
@@ -56,6 +60,9 @@ namespace IronMeta
             catch (Exception e)
             {
                 Console.Error.WriteLine(e.Message);
+                if (e.InnerException != null)
+                    Console.Error.WriteLine(e.InnerException.Message);
+
                 return 2;
             }
         }
