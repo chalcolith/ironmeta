175ecf2a41c6ea9f6071b446f15cbac40644270a
Gordon Tisher <gordon@balafon.net>
2015-06-26 22:41:55 -0700

    get rid of spurious file

--- Source/IronMeta/Properties/CommonAssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyCopyright("Copyright © Gordon Tisher 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("3.8.*")]
