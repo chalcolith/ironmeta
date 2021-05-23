b72b09eb56d39219c53e7d9858799dcfeac5fbb0
Stephen <stephen@7thg.com>
2021-05-23 01:40:08 -0400

    Adding in an initial benchmark project to start to get a baseline

---
+++ Source/IronMeta.Benchmarks/Benchmarks.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IronMeta.Benchmarks {
+
+    [SimpleJob(RuntimeMoniker.Net472, baseline: true)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp21)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp30)]
+    [SimpleJob(RuntimeMoniker.Net50)]
+    [RPlotExporter]
+    public class Benchmarks {
+
+        static string input;
+        static string GenerateInput(int count) => string.Join("+", Enumerable.Repeat("1", count));
+
+        [GlobalSetup]
+        public void GlobalSetup() => input = GenerateInput(5_000);
+
+        [Benchmark]
+        public void CalcParserBenchmark() {
+            var parser = new IronMeta.Samples.Calc.Calc();
+            var match = parser.GetMatch(input, parser.Expression);
+        }
+    }
+}
---
+++ Source/IronMeta.Benchmarks/Program.cs
@@ -0,0 +1,10 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace IronMeta.Benchmarks {
+    class Program {
+        static void Main(string[] args) {
+            var summary = BenchmarkRunner.Run<Benchmarks>();
+        }
+    }
+}
