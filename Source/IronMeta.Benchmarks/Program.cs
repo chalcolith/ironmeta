d005ba179a7ef6366ecb0a0d40b0f8a62d61d666
Steve <125539+steve7411@users.noreply.github.com>
2021-05-25 08:43:43 -0400

    Adding manual benchmark method runner for debugging purposes

--- Source/IronMeta.Benchmarks/Program.cs
+++ Source/IronMeta.Benchmarks/Program.cs
@@ -1,10 +1,21 @@
 ﻿using BenchmarkDotNet.Running;
 using System;
 
-namespace IronMeta.Benchmarks {
-    class Program {
-        static void Main(string[] args) {
+namespace IronMeta.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ManualRun();
             var summary = BenchmarkRunner.Run<Benchmarks>();
+        }
+
+        static void ManualRun()
+        {
+            var benchmark = new Benchmarks();
+            benchmark.GlobalSetup();
+            benchmark.CalcParserBenchmark();
         }
     }
 }
