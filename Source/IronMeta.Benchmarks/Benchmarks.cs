f81acfe081893ce5cf88db8158117205b19c7f47
Steve <125539+steve7411@users.noreply.github.com>
2021-05-25 08:34:02 -0400

    Updating the calc benchmark to use more of the parser features

--- Source/IronMeta.Benchmarks/Benchmarks.cs
+++ Source/IronMeta.Benchmarks/Benchmarks.cs
@@ -1,27 +1,65 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System;
+using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace IronMeta.Benchmarks {
+namespace IronMeta.Benchmarks
+{
 
     [SimpleJob(RuntimeMoniker.Net472, baseline: true)]
     [SimpleJob(RuntimeMoniker.NetCoreApp21)]
     [SimpleJob(RuntimeMoniker.NetCoreApp30)]
     [SimpleJob(RuntimeMoniker.Net50)]
     [RPlotExporter]
-    public class Benchmarks {
+    public class Benchmarks
+    {
+        private static List<char> inputList;
+        private static char[] inputArray;
+        private static string inputString;
 
-        static string input;
-        static string GenerateInput(int count) => string.Join("+", Enumerable.Repeat("1", count));
+        static IEnumerable<int> RandomInts(int count, int min, int max)
+        {
+            // We need this to generate the same numbers every time, so hard coding arbitrary seed
+            var rand = new Random(289759387);
+            for (int i = 0; i < count; i++)
+                yield return rand.Next(min, max);
+        }
+
+        static string GenerateInput(int count)
+        {
+            var rand = new Random(289759387);
+            var sb = new System.Text.StringBuilder(count * 5);
+            var ops = new[] { '+', '-', '*' };
+            foreach (var n in RandomInts(count, 0, 10_000))
+            {
+                sb.Append(n);
+                sb.Append(ops[rand.Next(0, ops.Length)]);
+            }
+            sb.Append('0');
+            return sb.ToString();
+        }
 
         [GlobalSetup]
-        public void GlobalSetup() => input = GenerateInput(5_000);
+        public void GlobalSetup()
+        {
+            inputString = GenerateInput(1_250);
+            inputList = inputString.ToList();
+            inputArray = inputString.ToArray();
+        }
 
         [Benchmark]
-        public void CalcParserBenchmark() {
+        public void CalcParserBenchmark()
+        {
             var parser = new IronMeta.Samples.Calc.Calc();
-            var match = parser.GetMatch(input, parser.Expression);
+            var match = parser.GetMatch(inputString, parser.Expression);
+            try
+            {
+                Console.WriteLine($"Result: {match.Result}");
+            } catch (Exception)
+            {
+                Console.Error.WriteLine($"Error: {match.Error}");
+            }
         }
     }
 }
